53d12c4b3af35ae0c4233944c03b444749fc9956
Turki Faisal <Turkif182@gmail.com>
2023-10-28 08:57:58 +0100

    Added a nav

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,10 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        primary: '#16232D',
+        secondary: '#EC661A',
+      },
     },
   },
   plugins: [],
