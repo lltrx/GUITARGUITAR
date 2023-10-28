732f5f156cdfacbcc956b4c31c21aac9dd700b35
2556961h <2556961h@student.gla.ac.uk>
2023-10-28 11:50:11 +0100

    broken

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,4 +1,57 @@
+"use client"
+
+import { useState } from "react"
+
 export default function Login() {
+
+  const [formState, setFormState] = useState({
+    email: '',
+    password: ''
+  })
+
+  const handleChange = (e) => {
+    setFormState({
+        ...formState,
+        [e.target.name]: e.target.value,
+    })
+  }
+
+  const handleSubmit = async (e) => {
+    console.log("hello")
+
+    e.preventDefault()
+
+    alert("submitted")
+
+    const response = await fetch('api/customers', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    const data = await response.json()
+
+    const user = data.find((user) => user.email === formState.email)
+
+    if (!user) {
+      alert('User not found')
+      return
+    }
+
+    if (formState.password !== "password") {
+      alert('Invalid password')
+      return
+    }
+
+
+    localStorage.put('user', user)
+
+    alert('Logged in successfully')
+  }
+
+
+
   return (
       <div className="relative flex top-0 left-0 w-screen h-screen">
         <video
@@ -12,27 +65,29 @@ export default function Login() {
         </video>
         <div className="absolute w-full h-full inset-0 bg-black bg-opacity-70 z-10" />
         <div className="absolute flex justify-center items-center w-screen h-screen top-0 left-0 z-20">
-          <div className="flex flex-col items-center justify-center bg-white text-black p-20 rounded-3xl">
+          <div className="flex flex-col items-center justify-center bg-primary text-white p-20 rounded-3xl">
           <div className="max-w-md mx-auto">
-				<div>
-					<h1 className="text-2xl text-center font-semibold">Login</h1>
-				</div>
-				<div className="divide-y divide-gray-200">
-					<div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
-						<div className="relative">
-							<input autoComplete="off" id="email" name="email" type="text" className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Email address" />
-							<label htmlFor="email" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Email Address</label>
-						</div>
-						<div className="relative">
-							<input autoComplete="off" id="password" name="password" type="password" className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Password" />
-							<label htmlFor="password" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Password</label>
-						</div>
-						<div className="relative mx-auto">
-							<button className="bg-blue-500 text-white rounded-md px-2 py-1">Submit</button>
-						</div>
-					</div>
-				</div>
-			</div>
+            <div>
+              <h1 className="text-2xl text-center text-white font-semibold">Login</h1>
+            </div>
+            <form onSubmit={handleSubmit}>
+              <div className="divide-y divide-gray-200">
+                <div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-base sm:leading-7">
+                  <div className="relative">
+                    <input autoComplete="off" id="email" name="email" type="text" value={formState.email} onChange={handleChange} className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600 rounded-3xl p-6" placeholder="Email address" />
+                    <label htmlFor="email" className="absolute pl-5 left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-3.5 transition-all peer-focus:-top-0.5 peer-focus:text-secondary peer-focus:text-sm">Email Address</label>
+                  </div>
+                  <div className="relative">
+                    <input autoComplete="off" id="password" name="password" type="password" value={formState.password} onChange={handleChange} className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600 rounded-3xl p-6" placeholder="Password" />
+                    <label htmlFor="password" className="absolute pl-5 left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-3.5 transition-all peer-focus:-top-0.5 peer-focus:text-secondary peer-focus:text-sm">Password</label>
+                  </div>
+                  <div className="flex relative justify-center pt-10">
+                    <button type="submit" className="bg-primary text-white border-secondary border-2 rounded-3xl px-4 py-1 hover:transition-all ease-in-out hover:bg-secondary duration-200">Log In</button>
+                  </div>
+                </div>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
 
