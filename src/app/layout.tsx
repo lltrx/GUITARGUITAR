17fb758ee179313c088bb3b1cdd6ca2903b03f04
2556961h <2556961h@student.gla.ac.uk>
2023-10-28 11:55:52 +0100

    broken

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,11 +16,11 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <>
-      <NavBar />
-      <main>
+    <html lang="en">
+      <body className={inter.className}>
+        <NavBar />
         {children}
-      </main>
-    </>
+      </body>
+    </html>
   )
 }
