6d04ac6df94f73809408058374a4a44ae397fe48
Naral <77269201+NaralC@users.noreply.github.com>
2023-10-28 21:31:57 +0100

    Styling Changes to OrderCard

--- src/app/components/orderCard.tsx
+++ src/app/components/orderCard.tsx
@@ -1,85 +1,85 @@
-'use client';
-import React from 'react';
-import { motion } from 'framer-motion';
-import Image from 'next/image';
-import Link from 'next/link';
-import { Button } from '../product/[SKU_ID]/page';
-import { StepperWithContent } from '@/components/StatusBar';
+"use client";
+import React from "react";
+import { motion } from "framer-motion";
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "../product/[SKU_ID]/page";
+import { StepperWithContent } from "@/components/StatusBar";
 
 export default function OrderCard({
-	orderNumber,
-	status,
-	orderDate,
-	deliveryAddress,
-	total,
-	onClick,
-	products,
+  orderNumber,
+  status,
+  orderDate,
+  deliveryAddress,
+  total,
+  onClick,
+  products,
 }) {
+  return (
+    <motion.div
+      onClick={onClick}
+      className="bg-white rounded-xl p-6 min-w-[500px] shadow-md "
+    >
+      <div className="flex justify-between mb-4">
+        <span className="text-gray-700 bg-slate-400 rounded-lg">
+          Order number: {orderNumber}
+        </span>
+      </div>
+      <div className="flex justify-between mb-4">
+        <span className="text-gray-700">Order Placed: {orderDate}</span>
+      </div>
+      <div className="mb-4 ">
+        <span className="text-lg font-bold text-black">Status:</span>
+        <span className="text-lg ml-2 text-black">{status}</span>
+        <StepperWithContent status={status} />
+      </div>
+      <div className="mb-4">
+        <span className="text-gray-700">Delivering to: {deliveryAddress}</span>
+      </div>
+      <div className="mt-4 border-t pt-4">
+        <span className="text-lg font-bold text-black">Total: £{total}</span>
+      </div>
+      <div className="mt-4 border-t pt-4">
+        <span className="text-lg font-bold text-black">Products: </span>
+        {products.map((product, i) => {
+          const truncatedDescription = `${product.ProductDetail.substring(
+            0,
+            25
+          )}...`;
 
-	return (
-		<motion.div
-			onClick={onClick}
-			className='bg-white rounded-xl p-6 min-w-[500px] shadow-md '>
-			<div className='flex justify-between mb-4'>
-				<span className='text-gray-700 bg-slate-400 rounded-lg'>
-					Order number: {orderNumber}
-				</span>
-			</div>
-			<div className='flex justify-between mb-4'>
-				<span className='text-gray-700'>Order Placed: {orderDate}</span>
-			</div>
-			<div className='mb-4 '>
-				<span className='text-lg font-bold text-black'>Status:</span>
-				<span className='text-lg ml-2 text-black'>{status}</span>
-				<StepperWithContent status={status} />
-			</div>
-			<div className='mb-4'>
-				<span className='text-gray-700'>Delivering to: {deliveryAddress}</span>
-			</div>
-			<div className='mt-4 border-t pt-4'>
-				<span className='text-lg font-bold text-black'>Total: £{total}</span>
-			</div>
-			<div className='mt-4 border-t pt-4'>
-				<span className='text-lg font-bold text-black'>Products: </span>
-				{products.map((product, i) => {
-					const truncatedDescription = `${product.ProductDetail.substring(
-						0,
-						50,
-					)}...`;
+          return (
+            <div key={i} className="flex flex-col">
+              <div className="flex flex-row">
+                <Image
+                  src={product.PictureMain}
+                  alt="Avatar"
+                  width={75}
+                  height={75}
+                  className="rounded-full object-cover"
+                />
+                <div className="flex flex-col p-5 gap-2 w-full">
+                  <span className="text-lg ml-2 text-black">
+                    {product.ItemName}
+                  </span>
+                  <span className="text-lg ml-2 text-black">
+                    Price: £{product.SalesPrice}
+                  </span>
+                  <span className="text-lg ml-2 text-black">
+                    Description: {truncatedDescription}
+                  </span>
 
-					return (
-						<div
-							key={i}
-							className='flex flex-col'>
-							<div className='flex flex-row'>
-								<Image
-									src={product.PictureMain}
-									alt='Avatar'
-									width={75}
-									height={75}
-									className='rounded-full object-cover'
-								/>
-								<span className='text-lg ml-2 text-black'>
-									{product.ItemName}
-								</span>
-							</div>
-							<span className='text-lg ml-2 text-black'>
-								Price: £{product.SalesPrice}
-							</span>
-							<span className='text-lg ml-2 text-black'>
-								Description: {truncatedDescription}
-							</span>
-							<button className='inline-flex items-center justify-center whitespace-nowrap rounded-md font-medium ring-offset-background bg-blue-500  hover:bg-blue-500/90 h-12 px-4 py-2 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 active:scale-95 transition-all text-lg '>
-								<Link
-									href={`http://localhost:3000/product/${product.SKU_ID}`}
-									className='text-white'>
-									More
-								</Link>
-							</button>
-						</div>
-					);
-				})}
-			</div>
-		</motion.div>
-	);
+                  <Link
+                    href={`http://localhost:3000/product/${product.SKU_ID}`}
+                    className="text-white"
+                  >
+                    <button className="inline-flex items-center justify-center whitespace-nowrap rounded-md font-medium ring-offset-background bg-blue-500  hover:bg-blue-500/90 h-12 px-4 py-2 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 active:scale-95 transition-all text-lg w-full">More</button>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </motion.div>
+  );
 }
--- src/components/loyalty.tsx
+++ src/components/loyalty.tsx
@@ -19,7 +19,7 @@ export default function Loyalty({ debug }: { debug?: boolean }) {
   return (
     <>
       <p className="text-4xl text-secondary">Loyalty Level 🎉</p>
-      <div className="w-full max-w-md md:max-w-3xl px-24 py-4 mb-32">
+      <div className="w-full max-w-3xl px-24 py-4 mb-32">
         <Stepper
           lineClassName="bg-black shadow shadow-white"
           activeLineClassName="bg-white shadow-lg shadow-white"
