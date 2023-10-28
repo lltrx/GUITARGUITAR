fa5869253c0c7b3e80c5f92fd0f65a4aebdfaf77
Ahmed Badawy <2613099b@student.gla.ac.uk>
2023-10-28 12:27:02 +0100

    Added filtering of orders based on userid in backend, need to pass customerID

--- src/app/api/orders/route.ts
+++ src/app/api/orders/route.ts
@@ -53,10 +53,27 @@ type OrderStatus =
 	| 'Completed'
 	| 'Cancelled';
 
-export async function GET(req: Request)  {
-	const response = await fetch(
-		'https://www.guitarguitar.co.uk/hackathon/orders/',
-	);
-	const data = await response.json();
-	return Response.json(data);
-}
+	export async function POST(req: any) {
+		try {
+			const request = await req.json();
+			const customerID = request.customerID;
+			const convertedCustomerID = Number(customerID);
+		
+			const response = await fetch('https://www.guitarguitar.co.uk/hackathon/orders/');
+			const data = await response.json();
+			const dataArray = Object.values(data);
+	
+			if (customerID) {
+				const filteredData = dataArray.filter((item: any) => item.CustomerId === convertedCustomerID);
+				if (filteredData.length === 0) {
+					return Response.json({ error: 'No orders found for this customer' });
+				}
+				return Response.json(filteredData);
+			}
+	
+			return Response.json(dataArray);
+		} catch (error) {
+			console.error('Error processing request:', error);
+			return Response.json({ error: error?.toString() });
+		}
+	}
